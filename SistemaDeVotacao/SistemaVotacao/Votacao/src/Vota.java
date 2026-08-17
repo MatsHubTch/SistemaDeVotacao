717c5993ff681a7033adf64ce7e221fc0f6122a8
Mateus Filipe Souza Martins <mateus.11881423@aluno.mg.gov.br>
2026-08-17 12:59:06 -0300

    cadastro de nome de candidatos, (foi o josé, mas pelo computaopdr do mateus

--- SistemaDeVotacao/SistemaVotacao/Votacao/src/Vota.java
+++ SistemaDeVotacao/SistemaVotacao/Votacao/src/Vota.java
@@ -7,28 +7,47 @@ class Vota {
     public static void main(String[] args) {
 
         Scanner teclado = new Scanner(System.in);
+
         int escolha = 0;
-        int quantia;
+        int quantia = 0;
+        String[] candidatos = null;
 
-        System.out.println("================================");
-        System.out.println("       SISTEMA DE VOTAÇÃO       ");
-        System.out.println("================================");
-        System.out.println("\n");
-        System.out.println("1 - Cadastro candidatos");
-        System.out.println("2 - Iniciar votação");
-        System.out.println("3 - Exibir resultado");
-        System.out.println("4 - Exibir matrix de votos");
-        System.out.println("5 - Encerrar sistema");
 
-        escolha = teclado.nextInt();
+//------------------------------------------------------------------------------------------
+
+        do {
+            System.out.println("================================");
+            System.out.println("       SISTEMA DE VOTAÇÃO       ");
+            System.out.println("================================");
+            System.out.println("\n");
+            System.out.println("1 - Cadastro candidatos");
+            System.out.println("2 - Iniciar votação");
+            System.out.println("3 - Exibir resultado");
+            System.out.println("4 - Exibir matrix de votos");
+            System.out.println("5 - Encerrar sistema");
+
+            escolha = teclado.nextInt();
+        } while (quantia > 5);
+
+//------------------------------------------------------------------------------------------
 
         if (escolha == 1) {
-        do {
-            System.out.println("Quantos candidatos deseja cadastrar?");
-            quantia = teclado.nextInt();
-        }while (quantia >= 6 );
 
 
+            do {
+
+                System.out.println("Quantos candidatos deseja cadastrar?");
+                quantia = teclado.nextInt();
+                teclado.nextLine();
+                candidatos = new String[quantia];
+                for (int i = 0; i < quantia; i++) {
+                    System.out.print("Qual o nome do candidato " + (i + 1) + "? ");
+                    candidatos[i] = teclado.nextLine();
+                }
+
+            } while (quantia >= 6);
+
+//------------------------------------------------------------------------------------------
 
         }
 
